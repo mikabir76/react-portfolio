9e6e267daa4a1f45bd803d15478462eccc347c4d
mikabir76 <kabirmd0987@gmail.com>
2023-07-07 23:30:22 +0600

    project done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     </div>
                    <div className='grid grid-cols-2'>
                     <div className='w-[30%]'>
-                        <img className='w-full md:w-60 backdrop-blur-lg border-8 border-gray-500' src={kabir} alt="" />
+                        <img className='w-full hidden md:block md:w-60 backdrop-blur-lg border-8 border-gray-500' src={kabir} alt="" />
                     </div>
                     <div className='w-full'>
                     <p>I am Mirajul Islam Kabir, I am a student. I am studying B.A Deegre at Govt. Dohar Nawabganj College. I started learning basic front end development from W3 school by my own interest. Then I also took an online course provided by Programming Hero to expand my knowledge in this field. One of my strengths is my ability to communicate and collaborate effectively with others. I enjoy working as part of a team and I'm always willing to learn new things. Additionally, I have skills in graphic design and Facebook marketing. When it comes to graphic design, I can create attractive and user-friendly designs for websites and other digital media. As for Facebook marketing, I know how to promote products and services, engage with the target audience, and analyze marketing campaigns for better results.</p>
